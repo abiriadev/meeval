e449f4af2245ebe9953381d866ef6f6adb253484
Abiria <abiria.dev@gmail.com>
2023-09-19 13:47:58 +0900

    test: add unit test for indeterminate form

--- tests/indeterminate.rs
+++ tests/indeterminate.rs
@@ -3,3 +3,8 @@ use meeval::eval;
 #[test]
 #[should_panic] // TODO: should show descriptive error message someday later
 fn divide_by_zero() { let _ = eval("123 / 0"); }
+
+#[test]
+// TODO: This answer is incorrect; it should raise an exception.
+// However, `pow` in Rust's standard library behaves this way, thus we allow it for now.
+fn zero_to_the_power_of_zero() { assert_eq!(eval("0 ^ 0"), Ok(1)) }
